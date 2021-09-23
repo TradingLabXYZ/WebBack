893fc989e12ed728d636b4fa7a056ec176f96ca0
robimalco <robimalco@gmail.com>
2021-09-23 16:51:35 +0200

    backend url

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func main() {
 		AllowedOrigins: []string{
 			"http://localhost:9000",
 			"https://fishsunset-fmujji.stormkit.dev",
-			"https://fishsunset-fmujji-44079058204449.stormkit.dev",
+			"https://fishsunset-fmujji-44088927018555.stormkit.dev",
 			"http://www.tradinglab.xyz",
 			"https://tradinglab.xyz",
 			"https://www.tradinglab.xyz",
