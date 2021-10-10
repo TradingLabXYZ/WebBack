9a4e3d388599cc0dd2be126a8d0d8731fb101c3e
robimalco <robimalco@gmail.com>
2021-10-10 18:41:16 +0200

    small refactor sessions and users

--- route_login.go
+++ route_login.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"time"
 
 	. "github.com/logrusorgru/aurora"
 	log "github.com/sirupsen/logrus"
@@ -48,26 +47,4 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	} else {
 		log.Info("Log in not valid user " + user.Email)
 	}
-}
-
-func (user *User) CreateSession() (session Session) {
-	fmt.Println(Gray(8-1, "Starting CreateSession..."))
-	session_sql := `
-		INSERT INTO sessions (uuid, email, userid, createdat)
-		VALUES ($1, $2, $3, $4)
-		RETURNING id, uuid, email, userid, createdat;`
-	DbWebApp.QueryRow(
-		session_sql,
-		createUUID(),
-		user.Email,
-		user.Id,
-		time.Now(),
-	).Scan(
-		&session.Id,
-		&session.Uuid,
-		&session.Email,
-		&session.UserId,
-		&session.CreatedAt,
-	)
-	return
 }
