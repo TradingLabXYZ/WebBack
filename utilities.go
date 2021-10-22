b657aaaa6e24280ef20b374a7aacfb5ecc603ba2
dolphinxyz <robimalco@gmail.com>
2021-10-22 06:15:10 +0200

    remove unused code

--- utilities.go
+++ utilities.go
@@ -4,8 +4,6 @@ import (
 	"crypto/rand"
 	"crypto/sha1"
 	"fmt"
-	"net/http"
-	"strings"
 
 	_ "github.com/lib/pq"
 )
@@ -29,30 +27,4 @@ func createUUID() (uuid string) {
 	u[6] = (u[6] & 0xF) | (0x4 << 4)
 	uuid = fmt.Sprintf("%x-%x-%x-%x-%x", u[0:4], u[4:6], u[6:8], u[8:10], u[10:])
 	return
-}
-
-func FormatRequest(r *http.Request) string {
-	// Create return string
-	var request []string
-	// Add the request string
-	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
-	request = append(request, url)
-	// Add the host
-	request = append(request, fmt.Sprintf("Host: %v", r.Host))
-	// Loop through headers
-	for name, headers := range r.Header {
-		name = strings.ToLower(name)
-		for _, h := range headers {
-			request = append(request, fmt.Sprintf("%v: %v", name, h))
-		}
-	}
-
-	// If this is a POST, add post data
-	if r.Method == "POST" {
-		r.ParseForm()
-		request = append(request, "\n")
-		request = append(request, r.Form.Encode())
-	}
-	// Return the request as a string
-	return strings.Join(request, "\n")
 }
